d043304461043c695fac50aa0deb476706c24e0c
Leonardo G Pivatto <leogpivatto@gmail.com>
2024-10-07 22:27:33 -0300

    initial config

---
+++ frontend/README.md
@@ -0,0 +1,8 @@
+Web app for a specific church
+-admin/user page
+-manage events/conferences/retreats
+
+.rsvp management
+.volunteer sign up
+.event scheduling
+.reminders
---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header></header>
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
